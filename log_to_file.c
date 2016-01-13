e824ab64fd771a3433abcb99084ecf9dfc464fe0
Valerio Cietto <valerio_ciet@hotmail.it>
2016-01-13 15:38:18 +0100

    Update log_to_file.c

--- log_to_file.c
+++ log_to_file.c
@@ -1,5 +1,10 @@
 #define LOG_FILE "/../../media/documents/home/valerio/Programmazione/Eclipse Mars Workspace/OspitalOS/src/log1.txt"
 
+/**
+ * log_to_file
+ * @param text text to append at the end of the log file
+ * LOG_FILE must be defined (default in configuration.h)
+ */
 void log_to_file(char* text){
 	FILE *f = fopen(LOG_FILE, "a");
 	//FILE *f = fopen("log1.txt", "a");
