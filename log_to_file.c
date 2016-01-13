c2a1bcafeebcd8824190d6611a01e86cfd699992
Valerio Cietto <valerio_ciet@hotmail.it>
2016-01-13 15:35:17 +0100

    Update log_to_file.c

--- log_to_file.c
+++ log_to_file.c
@@ -1,9 +1,16 @@
+#define LOG_FILE "/../../media/documents/home/valerio/Programmazione/Eclipse Mars Workspace/OspitalOS/src/log1.txt"
+
 void log_to_file(char* text){
-	FILE *f = fopen("LOG_FILE", "a");
+	FILE *f = fopen(LOG_FILE, "a");
+	//FILE *f = fopen("log1.txt", "a");
+
 	if (f == NULL){
+
+
 		printf("Error opening file!\n");
 		exit(1);
 	}
 	fprintf(f,text);
+	fprintf(f,"\n");
 	fclose(f);
 }
